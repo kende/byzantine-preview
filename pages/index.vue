a2e38f689ba9fac99330e65bb2d9b12c1761f2d9
Bearies <bearies@byzantine.network>
2018-03-14 23:37:24 -0400

    clean code

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,12 @@
   <div class="home-page">
     <div class="container">
       <div class="parallax-stars">
-        <!-- <div class="stars-group"> -->
-          <div id="layer1" class="layer"><img class="stars" src="~assets/bk-stars-1.png" alt="Star background"></div>
-          <div id="layer2" class="layer"><img class="stars" src="~assets/bk-stars-2.png" alt="Star background"></div>
-        <!-- </div> -->
+        <div id="layer1" class="layer"><img class="stars" src="~assets/bk-stars-1.png" alt="Star background"></div>
+        <div id="layer2" class="layer"><img class="stars" src="~assets/bk-stars-2.png" alt="Star background"></div>
       </div>
       <div class="content">
         <nav class="navbar">
-          <div class="nav-item"><a href="#"><img class="logo" src="~assets/Byzantine-Logo-yellow.png" alt=""></a></div>
+          <div class="nav-item"><a href="http://byzantine.network/"><img class="logo" src="~assets/Byzantine-Logo-yellow.png" alt=""></a></div>
           <div class="item-group">
             <div class="nav-item"><a href="#">START QUEST</a></div>
             <div class="nav-item"><a href="#">GET SPACE</a></div>
@@ -57,53 +55,15 @@
           <div class="purchase-btn"><button class="buy-btn">Buy Now</button></div>
         </section>
         <footer class="footer">
-          <div class='footer-item'><a href='http://byzantine.network/' target='_blank'>a byzantine thing</a> 🦄</div>
-          <div class='footer-item'><a href='https://app.termly.io/document/privacy-policy-for-website/c596a054-bb10-4bc0-b04d-d582f48ee43e' target='_blank'>privacy policy</a> &amp; <a href='https://app.termly.io/document/terms-of-use-for-website/afc96613-61ec-4c6d-89fa-c6720e93ffa1' target='_blank'>terms</a></div>
+          <div class="footer-item"><a href="http://byzantine.network/">a byzantine thing</a> 🦄</div>
+          <div class="footer-item"><a href="https://app.termly.io/document/privacy-policy-for-website/c596a054-bb10-4bc0-b04d-d582f48ee43e" target="_blank">privacy policy</a> &amp; <a href="https://app.termly.io/document/terms-of-use-for-website/afc96613-61ec-4c6d-89fa-c6720e93ffa1" target="_blank">terms</a></div>
         </footer>
       </div>
-      <!-- 
-
-      -->
-      
-      <!-- <div class="content">
-        <div class="logo-wrapper"><img class="logo" src="~assets/Byzantine-Logo.png" alt="Company logo"></div>
-        <div class="description">DECENTRALIZED PLATFORMS AND SPACES</div>
-      </div>
-      <footer class="footer">
-        cc Byzantine Inc. 2018
-      </footer> -->
-      <!-- <div class="page-content">
-        <div class="page-header">
-          <img class="geode-logo" src="~assets/geode-logo.png" alt="geode logo">
-          <div>BYZANTINE</div>
-        </div>
-        <div class="page-slogans">
-          <div class="slogan">A polychain cryptoeconomic network for shared discovery, creativity, and gameplay in any environment</div>
-        </div>
-        <div class="gallery-img-mobile">
-          <img src="~assets/byzantine-bk.png" alt="byzantine background">
-        </div>
-        <div class="page-footer">
-          <div class="copyright">
-            <div class="byzantine-thing">Byzantine Inc. 2018</div>
-          </div>
-        </div>
-      </div>
-      <div class="gallery-image">
-        <img src="~assets/byzantine-bk.png" alt="gallery images">
-      </div> -->
     </div>
   </div>
 </template>
 
 <script>
-// import AppLogo from '~/components/AppLogo.vue'
-
-// export default {
-//   components: {
-//     AppLogo
-//   }
-// }
 </script>
 
 <style scoped>
@@ -331,257 +291,4 @@
 .footer-item a:hover {
   text-decoration: underline;
 }
-
-
-
-
-
-
-
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-stify-content: center;
-  position: absolute;
-}
-
-
-
-.stars-wrapper {
-  
-}
-
-
-
-.stars {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.content {
-}
- .content {
-  flex: 1;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-.logo {
-  margin: auto;
-  width: 90%;
-  max-width: 760px;
-}
-.description {
-  margin-top: 20px;
-  font-family: "arame-regular", sans-serif;
-  font-size: 1.8em;
-  color: #424242;
-  letter-spacing: .2em;
-}
-
-.footer {
-  flex: 0;
-  text-align: center;
-  line-height: 60px;
-  font-size: .8em;
-  color: #8a8a8a;
-}
-
-@media (max-width: 860px) {
-  .description {
-    font-size: 1.5em;
-  }
-}
-@media (max-width: 668px) {
-  .description {
-    font-size: 1.2em;
-  }
-}
-.page-content {
-  flex: 1 1 auto;
-  padding: 24px;
-}
-.geode-logo {
-  height: 50px;
-}
-.page-slogans {
-  padding: 24px 0;
-  font-size: 3.4vw;
-}
-
-.input-group {
-  display: flex;
-  margin: 20px 0 50px;
-  width: 100%;
-  max-width: 400px;
-}
-.input-group input[type=email] {
-  flex: 1 1 auto;
-  vertical-align: top;
-  margin-right: -1px;
-  padding: 16px;
-  margin-top: 10px;
-  height: 40px;
-  width: 280px;
-  border: 1px solid #616161;
-}
-.input-group button {
-  flex: 0 0 auto;
-  vertical-align: top;
-  height: 40px;
-  width: 120px;
-  margin-top: 10px;
-  background: linear-gradient(45deg, #b1fdd8, #96f3ff, #c7b0fd, #f3cbe4);
-  border: 1px solid #616161;
-}
-.input-group input[type=submit] {
-  height: 100%;
-  width: 100%;
-  background: transparent;
-  border: 0;
-  font-family: "Avenir Next", Arial, Helvetica, sans-serif;
-  font-size: 1.2em;
-  font-weight: 500;
-  color: #5a5a5a;
-  letter-spacing: 3px;
-}
-
-.gallery-img-mobile {
-  display: none;
-  margin-left: -24px;
-  margin-right: -24px;
-}
-.gallery-img-mobile img {
-  width: 100%;
-}
-
-.app-info-wrapper {
-  position: relative;
-  z-index: 1;
-  margin-left: -24px;
-  margin-top: 18px;
-  padding: 24px;
-}
-.app-info-wrapper:before {
-  content: '';
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background: linear-gradient(45deg, #b1fdd8, #96f3ff, #c7b0fd, #f3cbe4);
-  opacity: .4;
-}
-.app-info {
-  position: relative;
-}
-.paragraph {
-  padding: 16px 0;
-  line-height: 1.4em;
-}
-.paragraph .linebreak {
-  display: block;
-}
-.paragraph a{
-  text-decoration: none;
-}
-.highlight {
-  color: #f466ac;
-}
-.byzantine-thing {
-  padding-top: 30px;
-}
-.byzantine-thing a {
-  text-decoration: none;
-}
-.creativecommons {
-
-}
-.creativecommons a {
-  text-decoration: none;
-}
-.credits {
-
-}
-.credits a {
-  text-decoration: none;
-}
-.app-info .line {
-  width: 100%;
-  max-width: 300px;
-  border: 1px solid #ddd;
-}
-
-.media-list {
-  margin-top: 24px;
-  margin-bottom: 10px;
-  padding: 0;
-  list-style: none;
-}
-.media-list li {
-  display: inline-block;
-  vertical-align: top;
-  margin-right: 16px;
-  height: 40px;
-}
-.media-list img {
-  height: 100%;
-}
-.copyright {
-  font-size: .7em;
-  color: #bdbdbd;
-}
-
-.gallery-image {
-  flex: 0 0 auto;
-  height: 100vh;
-  overflow: hidden;
-}
-.gallery-image img {
-  height: 100%;
-}
-
-@media (max-width: 940px) {
-  .page-slogans {
-    font-size: 6vw;
-  }
-  .gallery-img-mobile {
-    display: block;
-  }
-  .app-info-wrapper {
-    margin-right: -24px;
-  }
-  .gallery-image {
-    display: none;
-  }
-}
-@media (max-width: 786px) {
-  .page-slogans {
-    font-size: 7vw;
-  }
-  .slogan {
-    display: inline;
-  }
-}
-@media (max-width: 683px) {
-  .page-slogans {
-    font-size: 8vw;
-  }
-} */
 </style>
