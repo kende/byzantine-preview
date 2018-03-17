b78dfdba7022775adaf4900c56669cf2755935cc
Bearies <bearies@byzantine.network>
2018-03-17 17:24:23 -0400

    update navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,9 +2,9 @@
   <nav class="navbar">
     <div class="nav-item"><a href="http://byzantine.network/"><img class="logo" src="../assets/Byzantine-logo-yellow.png" alt="Byzantine logo"></a></div>
     <div class="item-group">
-      <div class="nav-item" v-if="currPage !== 'spacequest'"><nuxt-link to="/spacequest">HOME</nuxt-link></div>
-      <div class="nav-item" v-if="currPage !== 'startquest'"><nuxt-link to="/startquest">START QUEST</nuxt-link></div>
-      <div class="nav-item" v-if="currPage !== 'getspace'"><nuxt-link to="/getspace">GET SPACE</nuxt-link></div>
+      <div class="nav-item" v-if="currPage && currPage !== 'spacequest'"><nuxt-link to="/spacequest">HOME</nuxt-link></div>
+      <div class="nav-item" v-if="currPage && currPage !== 'startquest'"><nuxt-link to="/startquest">START QUEST</nuxt-link></div>
+      <div class="nav-item" v-if="currPage && currPage !== 'getspace'"><nuxt-link to="/getspace">GET SPACE</nuxt-link></div>
     </div>
   </nav>
 </template>
