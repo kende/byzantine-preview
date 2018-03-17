10fc3f483800324b60cf143d25a1dd54c7fbe651
Bearies <bearies@byzantine.network>
2018-03-17 17:34:20 -0400

    add side navbar

---
+++ components/SideNavbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <ul class="side-navbar">
+    <li class="side-nav-item" v-bind:class="{ active: currPage === 'startquest' }">
+      <nuxt-link to='/startquest'>Origin</nuxt-link>
+    </li>
+    <li class="side-nav-item" v-bind:class="{ active: currPage === 'startquest-getstarted' }">
+      <nuxt-link to='/startquest/getstarted'>Get Started</nuxt-link>
+    </li>
+    <li class="side-nav-item" v-bind:class="{ active: currPage === 'startquest-collect' }">
+      <nuxt-link to='/startquest/collect'>Collect</nuxt-link>
+    </li>
+    <li class="side-nav-item" v-bind:class="{ active: currPage === 'startquest-marketplace' }">
+      <nuxt-link to='/startquest/marketplace'>MarketPlace</nuxt-link>
+    </li>
+    <li class="side-nav-item" v-bind:class="{ active: currPage === 'startquest-FAQ' }">
+      <nuxt-link to='/startquest/FAQ'>FAQ</nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default { 
+  data () {
+    return {
+      currPage: null
+    }
+  },
+  mounted () {
+    this.currPage = $nuxt.$route.name
+    console.log(this.currPage)
+  },
+  watch: {
+    '$nuxt.$route' () {
+      this.currPage = $nuxt.$route.name
+    }
+  }
+}
+</script>
+
+<style scoped>
+.side-navbar {
+  width: 100%;
+}
+.side-nav-item {
+  margin: 20px;
+}
+.side-nav-item a {
+  font-family: "rational-light", sans-serif;
+  font-weight: 600;
+  color: #bdbdbd;
+  line-height: 1.6em;
+  text-decoration: none;
+  text-transform: uppercase;
+  letter-spacing: .1em;
+  transition: color .6s;
+}
+.side-nav-item.active a {
+  color: #f86bcf;
+}
+</style>
