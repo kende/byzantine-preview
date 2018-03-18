1455894a5ceaed5e94f17f0286bbd2a21037357e
Bearies <bearies@byzantine.network>
2018-03-17 21:37:13 -0400

    add content of get space page

--- pages/getspace.vue
+++ pages/getspace.vue
@@ -3,7 +3,44 @@
     <header class="page-header">
       <Navbar />
     </header>
-    <div class="content-wrapper"></div>
+    <div class="content-wrapper">
+      <div class="tile-info">
+        <div class="token-icon">
+          <img src="~assets/erc721-icon-blue.png" alt="ERC721 Token icon">
+        </div>
+        <div class="info">
+          <div class="tiles-left">100, 000 tiles left</div>
+          <div class="last-price">LAST TILE PRICE: Origin</div>
+          <div class="current-price">CURRENT PRICE: <span>.0215 ETH</span></div>
+          <div class="button-group">
+            <button class="buy1">BUY 1</button>
+            <button class="buy3">BUY 3</button>
+          </div>
+        </div>
+      </div>
+      <div class="detail">
+        <div class="detail-title">DETAILS</div>
+        <div class="detail-items">
+          <div class="detail-card">
+            <div class="detail-icon"><img src="~assets/arrow-icon.png" alt="arrow icon"></div>
+            <p class="detail-text">For each purchase, the price increases for the next tile by .0001 ETH.</p>
+          </div>
+          <div class="detail-card">
+            <div class="detail-icon"><img src="~assets/bundle-icon.png" alt="bundle icon"></div>
+            <p class="detail-text">If you purchase in a bundle, the price will be the same for all three.</p>
+          </div>
+          <div class="detail-card">
+            <div class="detail-icon"><img src="~assets/person-icon.png" alt="person icon"></div>
+            <p class="detail-text">Share with your friends to complete the quest!</p>
+            <div class="social-media">
+              <div class="social-icon"><img src="~assets/facebook-icon.png" alt=""></div>
+              <div class="social-icon"><img src="~assets/facebook-icon.png" alt=""></div>
+              <div class="social-icon"><img src="~assets/facebook-icon.png" alt=""></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="footer"><Footer /></div>
   </div>
 </template>
@@ -29,6 +66,7 @@ export default {
 
 .page-header {
   flex: 0 0;
+  padding-bottom: 200px;
   background-color: #1E226B;
   background-image: url('~assets/bk-stars-1.png'), url('~assets/bk-stars-2.png');
   background-size: 100%;
@@ -36,10 +74,174 @@ export default {
 }
 
 .content-wrapper {
+  padding: 0 20px;
+}
+
+.tile-info {
+  display: flex;
+  margin: -170px auto 0;
+  padding: 0 24px 40px;
+  width: 100%;
+  max-width: 800px;
+  border-bottom: 2px solid #eee;
+}
+.token-icon {
+  flex: 2;
+}
+.token-icon img {
+  height: 340px;
+  filter: drop-shadow(0px 0px 40px #ffe28c);
+}
+.info {
+  flex: 3;
+  margin-left: 40px;
+}
+.tiles-left {
+  margin-top: 100px;
+  line-height: 80px;
+  font-family: "arame-regular", sans-serif;
+  font-size: 38px;
+  color: #fff;
+}
+
+.last-price {
+  margin-top: 10px;
+  font-family: "rational-light", sans-serif;
+  font-size: .8em;
+  font-weight: 600;
+  color: #a5a5a5;
+}
+
+.content-wrapper {
   flex: 1 1;
+}
+.current-price {
+  margin: .4em 0;
+  font-family: "arame-regular", sans-serif;
+  font-size: 1.1em;
+  line-height: 2em;
+}
+.button-group {
+  margin-top: 20px;
+}
+.buy1,
+.buy3 {
+  padding: .7em 2em;
+  background: #f86bcf;
+  border: none;
+  font-family: "arame-regular", sans-serif;
+  font-size: 1em;
+  color: #fff;
+  letter-spacing: 2px;
+  cursor: pointer;
+}
+.buy3 {
+  margin-left: 20px;
+}
+
+.detail {
+  margin: 60px auto;
+  text-align: center;
+  width: 100%;
+  max-width: 1000px;
+}
+.detail-title {
+  margin: 80px 0 20px;
+  font-family: "arame-regular", sans-serif;
+  font-size:  1.4em;
+  font-weight: 500;
+  letter-spacing: .2em;
+}
+.detail-items {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.detail-card {
+  flex: none;
+  margin: 20px;
+  width: 258px;
+}
+.detail-icon {
+  margin: auto;
+  height: 40px;
+  width: 40px;
+}
+.detail-icon img {
+  height: 100%;
+  width: 100%;
+  object-fit: contain;
+}
+.detail-text {
+  margin: 20px 0;
+  font-family: "rational-light", sans-serif;
+  font-size: .8em;
+}
+
+.social-media {
+  display: flex;
+  justify-content: center;
+}
+.social-icon {
+  flex: none;
+  margin: 5px;
+  height: 30px;
+  width: 30px;
+}
+.social-icon img {
+  height: 100%;
+  width: 100%;
 }
 
 .footer {
   flex: 0 0;
+}
+
+@media (max-width: 804px) {
+  .page-header {
+    padding-bottom: 180px;
+  }
+  .tile-info {
+    margin: -150px auto 0;
+    max-width: 652px;
+  }
+  .token-icon img {
+    height: 300px;
+  }
+  .tiles-left {
+    margin-top: 80px;
+    font-size: 30px;
+  }
+}
+@media (max-width: 690px) {
+  .tile-info {
+    flex-wrap: wrap;
+    text-align: center;
+  }
+  .token-icon {
+    flex: none;
+    width: 100%;
+  }
+  .info {
+    margin: 0;
+    flex: none;
+    width: 100%;
+  }
+  .tiles-left {
+    margin-top: 40px;
+    color: #212121;
+  }
+}
+@media (max-width: 345px) {
+  .token-icon img {
+    height: auto;
+    width: 100%;
+  }
+  .buy1,
+  .buy3 {
+    margin: 10px auto;
+    display: block;
+    width: 120px;
+  }
 }
 </style>
