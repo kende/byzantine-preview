c5ceca08f1b5b74befb4c6b3801962650887c2f6
Bearies <bearies@byzantine.network>
2018-04-02 14:31:35 -0400

    change the page description

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A polychain cryptoeconomic network' }
+      { hid: 'description', name: 'description', content: 'Decentralized platforms and spaces' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
